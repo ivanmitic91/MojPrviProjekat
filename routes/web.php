01da3444a72f7096354a4de208d76ca94244c2d7
Ivan Mitic <ivan.mitic.10@singimail.rs>
2023-11-01 18:52:24 +0100

    fix home route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
+Route::view('/', 'contact')->name('home');
 Route::view('/contact', 'contact')->name('contact');
 Route::view('/about', 'about')->name('about');
 Route::view('/shop', 'shop')->name('shop');
